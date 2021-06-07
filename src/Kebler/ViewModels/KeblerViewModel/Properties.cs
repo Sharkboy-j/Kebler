1a0daaac8a7597ab4196f53c436375abdf4c2a89
Shark <sharkcffg@gmail.com>
2021-06-07 21:30:09 +0300

    one more fix

--- src/Kebler/ViewModels/KeblerViewModel/Properties.cs
+++ src/Kebler/ViewModels/KeblerViewModel/Properties.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
